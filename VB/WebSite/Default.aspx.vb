2133b5e7df18f97ef1258746078293a3bc24e284
DevExpressExampleBot <fakeEmail>
2022-02-07 17:43:49 +0300

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -10,7 +10,7 @@ Partial Public Class _Default
 			CmbCity.SelectedIndex = 0
 		End If
 	End Sub
-	Protected Sub CmbCity_Callback(ByVal source As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+	Protected Sub CmbCity_Callback(ByVal source As Object, ByVal e As DevExpress.Web.CallbackEventArgsBase)
 		FillCityCombo(e.Parameter)
 	End Sub
 	Protected Sub FillCityCombo(ByVal country As String)
